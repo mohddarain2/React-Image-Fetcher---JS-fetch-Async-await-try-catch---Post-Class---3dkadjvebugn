e20bcbb3f535b071b8cc3366aa4527143f58f8dd
mohddarain2 <mohddarain2@gmail.com>
2023-02-18 10:21:50 +0530

    daapak

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
+    const [image, setImage] = useState({});
+    const [loading, setLoading] = useState(false);
+    
+    const callApi = (number) => {
+        setLoading(true)
+        //
+        fetch('https://jsonplaceholder.typicode.com/photos/' + number)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setImage(data)
+                setLoading(false)
+            })
+    }
+    
+    useEffect(() => {
+        console.log('image - ', image);
+    }, [image])
+
+    const updateInput = (e) => {
+        console.log(e.target.value)
+        callApi(e.target.value)
+    }
+   
+    
+    return (
+        <>
+            Id number <input type="number" onChange={updateInput} />
+            {(loading) && <Loader />}
+            {(Object.keys(image).length > 0) && <PhotoFrame url={image.url} title={image.title} />
+            }
+        </>
+    )
 }
 
 
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = (props) => {
+    return (
+        <div className='photoframe'>
+            <img src={props.url} />
+            <div className='caption'>{props.title}</div>
+            
+        </div>
+    )
 }
